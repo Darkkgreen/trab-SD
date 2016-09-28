63b037fd78e71607cbbd902bb2c6542e80f5b67c
Ângela Rodrigues <angela.rod.fe@gmail.com>
2016-09-28 18:03:17 -0300

    Outra tentativa.. Exemplo

---
+++ Trabalho 1/outra-opcao/MulticastOrdenado/Message.java
@@ -0,0 +1,44 @@
+package MulticastOrdenado;
+
+import java.lang.Comparable;
+
+public class Message implements Comparable<Message>{
+	private int numero;
+	private int timeStamp;
+	
+	/**
+	 * Construtor
+	 * @param numero O numero da mensagem
+	 * @param timeStamp O tempo em que a mensagem foi enviada
+	 */
+	public Message(int numero, int timeStamp) {
+		this.numero = numero;
+		this.timeStamp = timeStamp;
+	}
+	
+	/**
+	 * getNumeroMensagem()
+	 * Retorna o número da mensagem
+	 */
+	public int getNumeroMensagem() {
+		return numero;
+	}
+	
+	/**
+	 * getTimeStamp()
+	 * Retorna o tempo em que a mensagem foi enviada
+	 */
+	public int getTimeStamp() {
+		return timeStamp;
+	}
+
+	public int compareTo(Message m2) {
+		if(this.timeStamp < m2.timeStamp)
+			return -1;
+		
+		if(this.timeStamp > m2.timeStamp)
+			return 1;
+		
+		return 0;
+	}
+}
---
+++ Trabalho 1/outra-opcao/MulticastOrdenado/MessageComparator.java
@@ -0,0 +1,10 @@
+package MulticastOrdenado;
+
+import java.util.Comparator;
+
+public class MessageComparator implements Comparator<Message> {
+	@Override
+	public int compare(Message m1, Message m2) {
+		return m1.compareTo(m2);
+	}
+}
