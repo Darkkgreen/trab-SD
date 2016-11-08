91b7b31efb14d67b8ee5be0edbdc607036a37fba
Ângela Rodrigues <angela.rod.fe@gmail.com>
2016-11-08 18:35:22 -0200

    Base para dar inicio

---
+++ Trabalho 3/MessageComp.java
@@ -0,0 +1,12 @@
+//Implementação
+//Ângela Rodrigues Ferreira - 552070
+//Gustavo Almeida Rodrigues - 489999
+
+import java.util.Comparator;
+
+public class MessageComp implements Comparator<Message>{
+	@Override
+	public int compare(Message atual, Message nova){
+		return atual.compareTo(nova);
+	}
+}
