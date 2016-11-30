9944b9217fe4aa66715d5f3e8c684b9c1800c452
darkkgreen <darkkgreen@github.com>
2016-11-30 02:38:01 -0200

    terminado de programar os dois arquivos

--- Trabalho 5/Client.java
+++ Trabalho 5/Client.java
@@ -1,14 +1,45 @@
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 
+import java.io.*;
+
 public class Client{
+	private CLient(){}
+
+	public boolean enviaArquivo(Archive stub, String nome){
+		FileInputStream fis = null;
+		File arquivo = new File("./"+ nome);
+		boolean retorno = false;
+
+		byte[] byteArquivo = new byte[(int) arquivo.length()];
+
+		try{
+			fis = new FileInputStream(arquivo);
+			fis.read(byteArquivo);
+			fis.close();
+
+			retorno = this.stub.uploadArchive(byteArquivo, nome);
+		}catch{
+			System.out.println("Problema com o envio do arquivo");
+		}
+
+		return retorno;
+	}
+
 	public static void main(String[] args) { 
-		String host = (args.length < 1) ? null : args[0]; 
+		String host = (args.length < 1) ? null : args[0];
+		String arquivo = (args.length < 2) ? null : args[1];
+
 		try { 
 			Registry registry = LocateRegistry.getRegistry(host); 
-			Hello stub = (Hello) registry.lookup("Hello"); 
-			String response = stub.sayHello();                
-			System.out.println("response: " + response); 
+			Archive stub = (Archive) registry.lookup("Archive");
+
+			boolean estado = enviaArquivo(stub, arquivo);
+
+			if(estado == true)
+				System.out.prinlnt("Arquivo " + arquivo + " enviado com sucesso");
+			else
+				System.out.println("Problema ao enviar arquivo");
 		} catch (Exception e) { 
 			System.err.println("Client exception: " + e.toString()); 
 			e.printStackTrace(); 
--- Trabalho 5/Server.java
+++ Trabalho 5/Server.java
@@ -3,22 +3,36 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import java.io.*;
+
 
 public class Server implements Archive{
 	public Server(){}
 	
-	public boolean uploadArchive(){
-		
+	public boolean uploadArchive(byte [] byteArchive, String nome){
+		System.out.println("Transferindo arquivo para servidor: " + nome);
+
+		try{
+			FileOutputStream fos = new FileOutputStream(nome);
+			fos.write(byteArchive);
+			fos.close();
+		}catch (IOException e){
+			System.out.println("Erro ao transferir arquivo");
+			return false;
+		}
+
+		System.out.println("Arquivo " + nome + " foi armazenado com sucesso");
+		return true;
 	}
 
 	public static void main(String args[]){
 		try{
 			Server obj = new Server(); 
-			Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);    
+			Archive stub = (Archive) UnicastRemoteObject.exportObject(obj, 0);    
 			Registry registry = LocateRegistry.getRegistry(); 
 			System.setProperty("java.rmi.server.hostname","127.0.0.1");
-			registry.bind("Hello", stub); 
-			System.err.println("Server ready"); 
+			registry.bind("Archive", stub); 
+			System.err.println("Servidor pronto para recebimento"); 
 		} catch(Exception e){
 			System.err.println("Server exception: "+ e.toString());
 			e.printStackTrace();
