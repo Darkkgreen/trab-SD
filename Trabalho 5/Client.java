d8aba2d9dd794e11dcf75057cd9ecbb5b702de89
Gustavo Rodrigues <grr.pride@gmail.com>
2016-11-30 03:39:31 -0200

    tentativa de modificar o client

--- Trabalho 5/Client.java
+++ Trabalho 5/Client.java
@@ -7,12 +7,21 @@ public class Client{
 	private Client(){}
 	private Archive stub;
 
-	public static boolean enviaArquivo(String nome){
+	public void criaStub(String host){
+		try {
+			Registry registry = LocateRegistry.getRegistry(host); 
+			this.stub = (Archive) registry.lookup("Archive");
+		}catch (Exception e){
+			System.out.println("Problema criando stub");
+		}
+	}
+
+	public boolean enviaArquivo(String nome){
 		FileInputStream fis = null;
 		File arquivo = new File("./"+ nome);
 		boolean retorno = false;
 
-		byte[] byteArquivo = new byte[(int) arquivo.length()];
+		byte [] byteArquivo = new byte[(int) arquivo.length()];
 
 		try{
 			fis = new FileInputStream(arquivo);
@@ -24,26 +33,21 @@ public class Client{
 			System.out.println("Problema com o envio do arquivo");
 		}
 
+		System.out.println("Arquivo enviado com sucesso!");
 		return retorno;
 	}
 
 	public static void main(String[] args) { 
 		String host = (args.length < 1) ? null : args[0];
 		String arquivo = (args.length < 2) ? null : args[1];
+		Client cliente = new Client();
 
-		try { 
-			Registry registry = LocateRegistry.getRegistry(host); 
-			stub = (Archive) registry.lookup("Archive");
+		cliente.criaStub(host);
+		boolean estado = cliente.enviaArquivo(arquivo);
 
-			boolean estado = enviaArquivo(arquivo);
-
-			if(estado == true)
-				System.out.println("Arquivo " + arquivo + " enviado com sucesso");
-			else
-				System.out.println("Problema ao enviar arquivo");
-		} catch (Exception e) { 
-			System.err.println("Client exception: " + e.toString()); 
-			e.printStackTrace(); 
-		}
+		if(estado == true)
+			System.out.println("Arquivo " + arquivo + " enviado com sucesso");
+		else
+			System.out.println("Problema ao enviar arquivo");
  	}
 } 
